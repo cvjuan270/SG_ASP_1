dbe4fa52635d2e1b9307ec6c26936c6103bb84c9
juan collado vasquez <diafron@hotmail.com>
2021-03-18 17:16:21 -0500

    medicina a m,edias

---
+++ Controllers/MedicinaController.cs
@@ -0,0 +1,71 @@
+﻿using SG_ASP_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SG_ASP_1.Controllers
+{
+    public class MedicinaController : Controller
+    {
+        private SG_ASP_1Context db = new SG_ASP_1Context();
+        // GET: Medicina
+        public ActionResult MedicinaCreate (int Id=2)
+        {
+            var aten = db.Atenciones.Find(Id);
+            var Medi = new MedicinaCreateViewModel();
+            Medi.AtenId = aten.Id;
+            Medi.oAteId = aten.Id;
+            Medi.TipExa = aten.TipExa;
+            Medi.NomApe = aten.NomApe;
+            Medi.DocIde = aten.DocIde;
+            Medi.Empres = aten.Empres;
+            //ViewBag.Medico = new SelectList(db.Medicos, "Medico", "Medico", atenciones.Medico);
+            ViewBag.Medico = new SelectList(db.Medicos, "Medico", "Medico", Medi.Medico);
+            return View(Medi);
+        }
+
+        [HttpPost]
+        public ActionResult MedicinaCreate(MedicinaCreateViewModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                Medicina med = new Medicina();
+                med.AtenId = model.oAteId;
+                med.HorIng = model.HorIng;
+                med.HorSal = model.HorSal;
+                med.Medico = model.Medico;
+                med.Aptitu = model.Aptitu;
+                med.FecApt = model.FecApt;
+                med.FecEnv = model.FecEnv;
+                med.Observ = model.Observ;
+                med.Coment = model.Coment;
+
+                db.Medicina.Add(med);
+                db.SaveChanges();
+
+                foreach (var item in model.interconsultas)
+                {
+                    var inte = new Interconsulta();
+                    inte.AtenId = model.AtenId;
+                    inte.IntCon = item.IntCon;
+                }
+
+                return RedirectToAction("Index", "Atenciones");
+
+            }
+            return View(model);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (true)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/MedicinaViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SG_ASP_1.Models
+{
+    public class MedicinaCreateViewModel
+    {
+        #region Atenciones
+        [Display(Name = "ID")]
+        public int oAteId { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Tip. Examen")]
+        public string TipExa { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Nombres y Apellidos")]
+        public string NomApe { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Doc. de Identidad")]
+        public string DocIde { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Empresa")]
+        public string Empres { get; set; }
+        #endregion
+
+        #region Medicina
+
+
+        public int AtenId { get; set; }
+
+        [Display(Name = "Hora de ingreso")]
+        [DataType(DataType.Time)]
+        public TimeSpan? HorIng { get; set; }
+
+        [Display(Name = "Hora de salida")]
+        [DataType(DataType.Time)]
+        public TimeSpan? HorSal { get; set; }
+
+        [StringLength(50)]
+        public string Medico { get; set; }
+
+        [StringLength(50,ErrorMessage = "El {0} debe contener como maximo 50 caracteres.")]
+        [Display(Name = "Aptitud")]
+        public string Aptitu { get; set; }
+
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Aptitud")]
+        public DateTime? FecApt { get; set; }
+
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de envio de resultados")]
+        public DateTime? FecEnv { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Comentarios")]
+        public string Coment { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Observaciones")]
+        public string Observ { get; set; }
+        #endregion
+
+        #region Interconsulta
+
+        public List<Interconsulta> interconsultas { get; set; }
+
+        #endregion
+    }
+
+    public class MedicinaDetailsViewModel
+    {
+        public string hola { get; set; }
+    }
+}
